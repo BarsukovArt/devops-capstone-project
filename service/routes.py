507498ede650ef6c75197568f9e23433cd672098
BarsukovArt <barsukov.art@gmail.com>
2023-03-28 07:42:42 -0400

    deleting account

--- service/routes.py
+++ service/routes.py
@@ -120,9 +120,20 @@ def update_account(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    Deletes an Account
+    This endpoint will delete an Account based on the posted data
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    
+    account = Account.find(account_id)
 
-# ... place you code here to DELETE an account ...
+    if account:
+        account.delete()
 
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
